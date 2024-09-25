7a5dee7c2ffcdeb37901e17fc26337155f0f46f0
Styvendev <styven.dev@gmail.com>
2024-09-24 21:40:17 -0500

    Add app logo and apply animation to logo

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.app;
 
+// Importar paquetes
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +19,10 @@ public class MainActivity extends AppCompatActivity {
 
         //metodo para mostrar o llamar una vista
         setContentView(R.layout.activity_main);
+
+        ImageView container = findViewById(R.id.imgLogo);
+        Animation animacion = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_out);
+        View vista = findViewById(R.id.imgLogo);
+        vista.startAnimation(animacion);
     }
 }
